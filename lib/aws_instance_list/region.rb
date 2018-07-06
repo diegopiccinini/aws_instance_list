82fcd56a9501f5f3e4c94dc8e92b1689083fed19
Diego PL <diego.piccinini@gmail.com>
2018-07-06 10:17:26 +0100

    region list tested

--- lib/aws_instance_list/region.rb
+++ lib/aws_instance_list/region.rb
@@ -10,6 +10,16 @@ module AwsInstanceList
         @ec2||=Aws::EC2::Client.new
       end
 
+      def descriptions
+        @descriptions||=ec2.describe_regions
+      end
+
+      def list
+        descriptions.regions.map do |description|
+          description.region_name
+        end
+      end
+
     end
 
   end
--- spec/lib/region_spec.rb
+++ spec/lib/region_spec.rb
@@ -4,4 +4,8 @@ describe AwsInstanceList::Region do
 
   it { expect(AwsInstanceList::Region.ec2).to be_a Aws::EC2::Client }
 
+  it { expect(AwsInstanceList::Region.descriptions.regions).to be_a Array }
+
+  it { expect(AwsInstanceList::Region.list).to include 'eu-west-1' }
+
 end
