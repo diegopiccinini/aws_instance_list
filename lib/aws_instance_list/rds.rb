3f81f5aa243fad6084b344d37706d822c5923335
Diego PL <diego.piccinini@gmail.com>
2018-07-06 13:28:34 +0100

    added db_list methods

--- lib/aws_instance_list/rds.rb
+++ lib/aws_instance_list/rds.rb
@@ -3,18 +3,29 @@ require 'aws-sdk-rds'
 module AwsInstanceList
 
   class RDS
-    attr_accessor :client
 
-    def initialize
-      @client=Aws::RDS::Client.new
+    attr_accessor :client, :region
+
+    def initialize region: 'eu-west-1'
+
+      @region=region
+
+      @client=Aws::RDS::Client.new region: region
+
     end
 
-    def db_descriptions
-      @db_descriptions||=client.describe_db_instances
+    def db_descriptions options={}
+      @db_descriptions||=client.describe_db_instances(options)
     end
 
-    def db_instances
-      db_descriptions.db_instances
+    def db_instances options={}
+      @db_intances||=db_descriptions(options).db_instances
+    end
+
+    def db_list fields=[:db_name,:engine,:allocated_storage, :db_instance_status]
+      db_instances.map do |i|
+        fields.map { |f| i.send(f) } << region
+      end
     end
 
   end
--- spec/lib/rds_spec.rb
+++ spec/lib/rds_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe RDS do
 
+  let(:db_instances) { subject.db_instances({ max_records: 20}) }
+
   it { expect(subject.client).to be_a Aws::RDS::Client }
 
-  it { expect(subject.db_instances).to be_a Array }
+  it { expect(db_instances.count).to eq(20)  }
+
+  it { expect(subject.db_list).to be_a Array }
+
+  #after { puts subject.db_list }
 
 end
